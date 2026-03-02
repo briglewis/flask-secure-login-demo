5a7618c94d67832b5ad96968508e70e40c0065e0
Kiki <Kiki_Argentina>
2026-03-02 13:36:03 +1100

    added more secure secret key fallback

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+        raise ValueError('SECRET_KEY must be set')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL') or \
